d2001521f0d30a0b1925e1b0bd6f9e1be7ebae5e
DESKTOP-EP6SMQC\auddn <auddn521@gmail.com>
2022-03-07 13:57:58 +0900

    init

---
+++ src/main/java/com/example/inoutkrwtemplate/InoutKrwTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.example.inoutkrwtemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InoutKrwTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InoutKrwTemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/inoutkrwtemplate/InoutKrwTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inoutkrwtemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InoutKrwTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
